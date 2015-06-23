68bea117c07c01b8460f8c111b6587b7e256ce3a
Jim Tryon <tryonjf@gmail.com>
2015-06-22 23:03:41 -0700

    Fixed merge conflict

--- programs-process.php
+++ programs-process.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -92,37 +91,3 @@
 	</script>
 	</body>
 </html>
-=======
-<?
-// Display table for IT plan
-$submitted = isset($_POST['submit']);
-
-// when form is submitted, include the correct schedule
-if ($submitted) {
-	$prog = $_POST['program'];
-	$a_qtr = $_POST['aa-quarter'];
-	$b_qtr = $_POST['bas-quarter'];
-
-	//$class = $_POST['class'];
-
-	// if bas program is selected, modify the sched string
-	if ($prog == "bas-network" || $prog == "bas-software") {
-		$sched = $prog. '-' .$b_qtr;
-
-		//echo $sched;
-	} else {
-		$sched = $prog. '-' .$a_qtr;
-	}
-
-    $path = "schedules/";
-
-	include_once ($path . $sched . '.php');
-
-	if ($prog == "aa-systems") {
-		include_once('track-key.php');
-	}
-
-	include_once('modal-process.php');
-}
-?>
->>>>>>> 12638163adf82aae0167de4b89f76fe8447d9bc4
