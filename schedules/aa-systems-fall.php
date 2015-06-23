cbbe33c700dc7d0cce438ff269f6c9ffd6a6baab
Jim Tryon <tryonjf@gmail.com>
2015-06-23 11:32:22 -0700

    Fixed courses in schedule

--- schedules/aa-systems-fall.php
+++ schedules/aa-systems-fall.php
@@ -1,75 +1,97 @@
 <title>AAS-T IT Systems Fall Start</title>
-
 <div class="table">
-<caption><h2>AAS-T IT Systems Fall Start</h2></caption>
-<tr><h3>1st Year</h3></tr>
-
-<table class="table table-bordered">
-<thead>
-<tr>
-	<th>Fall</th>
-	<th>Winter</th>
-	<th>Spring</th>
-	<th>Summer</th>
-</tr>
-</thead>
-
-<tbody>
-<tr class="network">
-	<td><a href="../../advising//modal.php?courseid=it141" data-toggle="modal" data-target="#myModal">IT141</a></td>
-	<td><a href="../../advising//modal.php?courseid=it160" data-toggle="modal" data-target="#myModal">IT160</a></td>
-	<td><a href="../../advising//modal.php?courseid=it201" data-toggle="modal" data-target="#myModal">IT201</a></td>
-	<td>&nbsp;</td>
-</tr>
-<tr class="software">
-	<td><a href="../../advising/modal.php?courseid=it114" data-toggle="modal" data-target="#myModal">IT114</a></td>
-	<td><a href="../../advising/modal.php?courseid=it131" data-toggle="modal" data-target="#myModal">IT131</a></td>
-	<td><a href="../../advising/modal.php?courseid=it135" data-toggle="modal" data-target="#myModal">IT135</a></td>
-	<td><a href="../../advising/modal.php?courseid=it190" data-toggle="modal" data-target="#myModal">IT190</a></td>
-</tr>
-
-
-</tbody>
-</table>
-
-
-<tr><h3>2nd Year</h3></tr>
-
-<table class="table table-bordered">
-<thead>
-<tr>
-	<th>Fall</th>
-	<th>Winter</th>
-	<th>Spring</th>
-	<th>Summer</th>
-</tr>
-</thead>
-
-<tbody>
-
-<tr>
-	<td><a href="../../advising//modal.php?courseid=it102" data-toggle="modal" data-target="#myModal">IT102</a></td>
-	<td><a href="../../advising//modal.php?courseid=it240" data-toggle="modal" data-target="#myModal">IT240</a></td>
-	<td><a href="../../advising/modal.php?courseid=it236" data-toggle="modal" data-target="#myModal">IT 236</a></td>
-	<td>&nbsp;</td>
-</tr>
-
-
-
-<tr class="network">
-	<td><a href="../../advising//modal.php?courseid=it210" data-toggle="modal" data-target="#myModal">IT210</a></td>
-	<td><a href="../../advising//modal.php?courseid=it243" data-toggle="modal" data-target="#myModal">IT243</a></td>
-	<td><a href="../../advising//modal.php?courseid=it245" data-toggle="modal" data-target="#myModal">IT245</a></td>
-	<td>&nbsp;</td>
-</tr>
-
-
-
-<tr class="software">
-	<td><a href="../../advising//modal.php?courseid=it121" data-toggle="modal" data-target="#myModal">IT121</a></td>
-	<td><a href="../../advising//modal.php?courseid=it219" data-toggle="modal" data-target="#myModal">IT219</a></td>
-	<td><a href="../../advising//modal.php?courseid=it220" data-toggle="modal" data-target="#myModal">IT220</a></td>
-	<td>&nbsp;</td>
-</tr>
-</tbody>
-</table>
+    <caption>
+        <h2>AAS-T IT Systems Fall Start</h2>
+    </caption>
+    <tr>
+        <h3>1st Year</h3>
+    </tr>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Fall</th>
+                <th>Winter</th>
+                <th>Spring</th>
+                <th>Summer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it114" data-toggle="modal" data-target="#myModal">IT114</a>
+                </td>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it131" data-toggle="modal" data-target="#myModal">IT131</a>
+                </td>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it135" data-toggle="modal" data-target="#myModal">IT135</a>
+                </td>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it190" data-toggle="modal" data-target="#myModal">IT190</a>&nbsp;
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it141" data-toggle="modal" data-target="#myModal">IT141</a>
+                </td>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it160" data-toggle="modal" data-target="#myModal">IT160</a>
+                </td>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it201" data-toggle="modal" data-target="#myModal">IT201</a>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </tbody>
+    </table>
+    <tr>
+        <h3>2nd Year</h3>
+    </tr>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Fall</th>
+                <th>Winter</th>
+                <th>Spring</th>
+                <th>Summer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <a href="../../advising//modal.php?courseid=it102" data-toggle="modal" data-target="#myModal">IT102</a>
+                </td>
+                <td>
+                    <a href="../../advising//modal.php?courseid=it240" data-toggle="modal" data-target="#myModal">IT240</a>
+                </td>
+                <td>
+                    <a href="../../advising/modal.php?courseid=it236" data-toggle="modal" data-target="#myModal">IT 236</a>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr class="network">
+                <td>
+                    <a href="../../advising//modal.php?courseid=it210" data-toggle="modal" data-target="#myModal">IT210</a>
+                </td>
+                <td>
+                    <a href="../../advising//modal.php?courseid=it243" data-toggle="modal" data-target="#myModal">IT243</a>
+                </td>
+                <td>
+                    <a href="../../advising//modal.php?courseid=it245" data-toggle="modal" data-target="#myModal">IT245</a>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr class="software">
+                <td>
+                    <a href="../../advising//modal.php?courseid=it121" data-toggle="modal" data-target="#myModal">IT121</a>
+                </td>
+                <td>
+                    <a href="../../advising//modal.php?courseid=it219" data-toggle="modal" data-target="#myModal">IT219</a>
+                </td>
+                <td>
+                    <a href="../../advising//modal.php?courseid=it220" data-toggle="modal" data-target="#myModal">IT220</a>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </tbody>
+    </table>
